150baa995a9edeabfdea0f947b3f62ab551f33a4
JooJangBeen <jangbeen92@naver.com>
2024-09-11 21:11:27 +0900

    사운드 #1

---
+++ ProjectUR/Source/ProjectUR/Public/11_Manager/SoundManager.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class PROJECTUR_API SoundManager
+{
+public:
+	SoundManager();
+	~SoundManager();
+};
