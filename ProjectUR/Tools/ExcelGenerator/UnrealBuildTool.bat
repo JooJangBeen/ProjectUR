37ef4e7ff1eeb66d25d0b6add6809990170130eb
Parkeunhyuk <peh960725@naver.com>
2024-09-19 23:04:07 +0900

    엑셀 매니저 #5
    엑셀 매니저 작업 완료

--- ProjectUR/Tools/ExcelGenerator/UnrealBuildTool.bat
+++ ProjectUR/Tools/ExcelGenerator/UnrealBuildTool.bat
@@ -1,7 +1,6 @@
 @echo off
 
-
 REM Generate Visual Studio project files 실행
-%UE5_PATH%\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.exe -projectfiles -project=%PROJECT_PATH% -game -engine -progress
+%UE5_PATH%\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.exe -projectfiles -project=%UPROJECT_PATH% -game -engine -progress
 
 pause
