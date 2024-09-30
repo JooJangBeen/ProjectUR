95e776ea6895653eca4a9a2cb1a9d114be7af6cc
JooJangBeen <jangbeen92@naver.com>
2024-09-30 22:21:11 +0900

    데디케이티드서버 구축 #6projectURClient.Target 클래스 추가

--- ProjectUR/Source/ProjectURServer.Target.cs
+++ ProjectUR/Source/ProjectURServer.Target.cs
@@ -8,7 +8,8 @@ public class ProjectURServerTarget : TargetRules
 	public ProjectURServerTarget(TargetInfo Target) : base(Target)
 	{
         Type = TargetType.Server;
-        DefaultBuildSettings = BuildSettingsVersion.V2;
+        DefaultBuildSettings = BuildSettingsVersion.V5;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
         ExtraModuleNames.Add("ProjectUR"); //이 행은 프로젝트 이름에 맞게 변경합니다.
     }
 }
