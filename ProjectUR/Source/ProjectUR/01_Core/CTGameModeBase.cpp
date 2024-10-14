ab45e73e95a9b693e83fb88bb7452464eed14a84
JooJangBeen <jangbeen92@naver.com>
2024-10-14 23:12:07 +0900

    서버 작업 #1

--- ProjectUR/Source/ProjectUR/01_Core/CTGameModeBase.cpp
+++ ProjectUR/Source/ProjectUR/01_Core/CTGameModeBase.cpp
@@ -7,6 +7,7 @@
 #include "EnhancedInputComponent.h"
 #include "CTWorldSettings.h"
 #include "EnhancedInputSubsystems.h"
+#include "../ProjectUR.h"
 
 ACTGameModeBase::ACTGameModeBase() : Super::AGameModeBase()
 {
@@ -59,8 +60,49 @@ ACTGameModeBase::ACTGameModeBase() : Super::AGameModeBase()
 
 void ACTGameModeBase::BeginPlay()
 {
+	Super::BeginPlay();
 
+}
 
+//클라이언트의 접속요청을 처리하는 함수
+void ACTGameModeBase::PreLogin(const FString& Options, const FString& Address, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage)
+{
+	PUR_LOG(LogNetwork, Log, TEXT("% s"), TEXT("Begin"));
+
+	Super::PreLogin(Options, Address, UniqueId, ErrorMessage);
+
+	PUR_LOG(LogNetwork, Log, TEXT("% s"), TEXT("End"));
+}
+
+//접속을 허용한 클라이언트에 대응하는 플레이컨트롤러를 만드는 함수
+APlayerController* ACTGameModeBase::Login(UPlayer* NewPlayer, ENetRole InRemoteRole, const FString& Portal, const FString& Options, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage)
+{
+	PUR_LOG(LogNetwork, Log, TEXT("% s"), TEXT("Begin"));
+
+	APlayerController* NewPlayerController = Super::Login(NewPlayer, InRemoteRole, Portal, Options, UniqueId, ErrorMessage);
+	
+	PUR_LOG(LogNetwork, Log, TEXT("% s"), TEXT("End"));
+	return NewPlayerController;
+}
+
+//플레이어 입장을 위해 플레이어에 필요한 기본 설정을 모두 마무리하는 함수
+void ACTGameModeBase::PostLogin(APlayerController* NewPlayer)
+{
+	PUR_LOG(LogNetwork, Log, TEXT("% s"), TEXT("Begin"));
+
+	Super::PostLogin(NewPlayer);
+	
+	PUR_LOG(LogNetwork, Log, TEXT("% s"), TEXT("End"));
+}
+
+//게임의 시작을 지시하는 함수
+void ACTGameModeBase::StartPlay()
+{
+	PUR_LOG(LogNetwork, Log, TEXT("% s"), TEXT("Begin"));
+
+	Super::StartPlay();
+
+	PUR_LOG(LogNetwork, Log, TEXT("% s"), TEXT("End"));
 }
 
 
--- ProjectUR/Source/ProjectUR/ProjectUR.h
+++ ProjectUR/Source/ProjectUR/ProjectUR.h
@@ -3,3 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+#define LOG_CALLINFO ANSI_TO_TCHAR(__FUNCTION__)
+// ...가변인자를 받는것이다. 로그 매크로 선언
+#define PUR_LOG(LogCat,		Verbosity, Format, ...)  UE_LOG(LogCat, Verbosity, TEXT("%s %s"), LOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__))
+
+DECLARE_LOG_CATEGORY_EXTERN(LogNetwork, Log, All);//네트워크 로그 카테고리 지정
